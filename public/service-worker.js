2e3da0a0478210ca3383ec66f3fbd3e0079a59a2
Samra <hismm8690@gmail.com>
2025-10-29 22:52:39 -0700

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open("habit-tracker-cache").then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return (
+          response ||
+          fetch(event.request).then((networkResponse) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+        );
+      });
+    })
+  );
+});
