155acb05e76f765e7b3ff486f64f5d519864e5de
Samra <hismm8690@gmail.com>
2025-10-29 22:38:47 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', () => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Optional: cache requests here for offline use
-});
-
